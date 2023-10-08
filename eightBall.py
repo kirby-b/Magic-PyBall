34c25689bffc24b573b5fb29e37b2bf07f4b39c4
kirby-b <kirby.brodie66@gmail.com>
2023-10-08 14:39:01 -0400

    Update eightBall.py

--- eightBall.py
+++ eightBall.py
@@ -10,11 +10,11 @@ def main():
         shakeEightBall()
 
 def shakeEightBall():
-    answers = ["It is certain", "Reply hazy, try again", "As I see it, yes", "Don't count on it", 
-                "It is decidedly so", "Ask again later", "Most likely", "My reply is no", 
-                "Without a doubt", "Better not tell you now", "Outlook good", "My sources say no",
-                "Yes definitely", "Cannot predict now", "Yes", "Outlook not so good",
-                "You may rely on it", "Concentrate and ask again", "Signs point to yes", "Very doubtful"]
+    answers = ["It is certain", "Reply hazy, try again", "As I see it, yes", "Don't count on it", "Ask someone else",
+                "It is decidedly so", "Ask again later", "Most likely", "My reply is no", "Wubba Lubba Dub Dub", 
+                "Without a doubt", "Better not tell you now", "Outlook good", "My sources say no", "Outlook awful"
+                "Yes definitely", "Cannot predict now", "Yes", "Outlook not so good", "Please wipe the screen, I couldnt hear you"
+                "You may rely on it", "Concentrate and ask again", "Signs point to yes", "Very doubtful", "Perhaps therapy"]
     print("You did it")
 
 if __name__ == "__main__":
